fc1a82ebb59cbfa884dd074162e4b6147fb11639
Atul Preetham <apreetham@dons.usfca.edu>
2021-10-05 10:29:59 -0700

    Modified code to include loop

--- code/01_data_subset_to_state.R
+++ code/01_data_subset_to_state.R
@@ -2,16 +2,26 @@
 # to just include rows from any particular US state and then write out a
 # csv file that only has that subset
 # uses function in the code/function directory
+# inserted a for loop for subsetting data for any 5 US states
 
 
 # Atul Preetham
 # apreetham@dons.usfca.edu
-# September 22th, 2021
+# October 5th, 2021
 
 # load functions
 source("code/functions/subset_mobility_data_to_state.R")
 
-# testing the use of function
+# testing the function
 subset_mobility_data_to_state(
   input_file_name = "data/raw_data/applemobilitytrends-2021-10-03.csv",
-  state_to_subset = "Florida")
+  state_to_subset = "Texas")
+
+# using the function to subset data of any 5 US states of choice through the
+# usage of for loop
+for (state_to_subset in c("California", "Texas", "Kansas",
+                          "Michigan", "Florida")) {
+  source("code/functions/subset_mobility_data_to_state.R")
+  input_file_name = "data/raw_data/applemobilitytrends-2021-10-03.csv"
+  subset_mobility_data_to_state(input_file_name,state_to_subset)
+}
