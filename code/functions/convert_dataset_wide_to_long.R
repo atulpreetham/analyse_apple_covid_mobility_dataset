8d6c51b299625744d4e5977088303540d611da73
Atul Preetham <apreetham@dons.usfca.edu>
2021-10-24 13:32:21 -0700

    Cleared lintr errors

--- code/functions/convert_dataset_wide_to_long.R
+++ code/functions/convert_dataset_wide_to_long.R
@@ -8,16 +8,16 @@
 # Create a function to convert the mobility data for a particular state from
 # wide to long
 wide_to_long <- function(input_subsetted_state) {
-  
+
   # loading required packages
   library(tidyr)
   library(dplyr)
   library(readr)
-  
+
   # input for the function is the output from the subset_mobility_data_to_state
   # function
   state_to_convert <- read_csv(file = input_subsetted_state)
-  
+
   # defensive programming check to make sure input data is correct
   defense_check <- nrow(state_to_convert)
   if (defense_check == 0) {
@@ -28,7 +28,7 @@ wide_to_long <- function(input_subsetted_state) {
                                       names_to = "Date",
                                       values_to = "mobility_value")
   #output
-  write_csv(subsetted_data_long, 
+  write_csv(subsetted_data_long,
             file = paste0("output/output_mobility_data_long/",
                           tools::file_path_sans_ext(
                             basename(input_subsetted_state)),
