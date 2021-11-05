1d13a4dc56e9dd3b9a27b2460b8d0897eef2068f
Atul Preetham <apreetham@dons.usfca.edu>
2021-11-04 18:16:17 -0700

    Add output dir and filename change options

--- run_analyse_covid_pipeline.sh
+++ run_analyse_covid_pipeline.sh
@@ -17,6 +17,12 @@ then
   echo "The second is the path to the mobility data csv file."
   exit 1
 fi
+
+state=$1
+state=${state// /_}
+
 Rscript -e "rmarkdown::render('Analysis.Rmd', \
             params = list(state = '$1', \
-            data = '$2'))"
+            data = '$2'),\
+            output_dir = 'output',\
+            output_file = 'Analysis_$state')"
